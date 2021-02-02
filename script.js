729b244536c28fb8e8edcd0aec04fa16c50e2b4a
dsoyuyuce18 <56314233+dsoyuyuce18@users.noreply.github.com>
2021-02-02 14:18:16 +0300

    HTML, CSS, JS project

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-console.log("TEST");
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
+
+// Get Quote From API
+async function getQuote() {
+  
+    const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // If Author is blank, add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        // Stop Loader, Show Quote
+        complete();
+    } catch (error) {
+        getQuote();
+    }
+}
+
+// twitter 
+function tweetQuote(){
+    const quote = quoteText.innerText ;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl , '_blank');
+
+}
+//
+newQuoteBtn.addEventListener('click',getQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
+// ON LOAD
+getQuote();
